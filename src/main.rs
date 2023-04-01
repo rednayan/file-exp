d75ad74f46e2ee4f034e65fb2945b75c76b19a52
nayan <nayansharma1010@gmail.com>
2023-04-01 13:38:30 +0530

    feat:add anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,31 @@
+use anyhow::Result;
 use std::{
     env,
     fs::{self, Metadata},
     path::{Path, PathBuf},
 };
 
-fn main() {
+fn main() -> Result<()> {
     let mut arg_vec = Vec::new();
     for args in env::args() {
         arg_vec.push(args);
     }
     let path = String::from(&arg_vec[1]);
-    visit_dir(&PathBuf::from(&path));
+    visit_dir(&PathBuf::from(&path))?;
+    Ok(())
 }
 
-fn visit_dir(path: &Path) {
-    let directories = fs::read_dir(path).unwrap();
+fn visit_dir(path: &Path) -> Result<()> {
+    let directories = fs::read_dir(path)?;
     for entry in directories {
-        let entry = entry.unwrap();
+        let entry = entry?;
         let subpath = entry.path();
         if subpath.is_dir() {
             println!("subpath: {:?}", &subpath);
-            visit_dir(&subpath);
+            visit_dir(&subpath)?;
         }
-        let metadata: Metadata = fs::metadata(&subpath).unwrap();
+        let metadata: Metadata = fs::metadata(&subpath)?;
         // println!("{:?}", subpath);
     }
+    Ok(())
 }
