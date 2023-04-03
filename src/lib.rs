7c5854f3497ab4ea3185fd42976f9bfdc3359d9f
nayan <nayansharma1010@gmail.com>
2023-04-03 18:23:16 +0530

    debugging sorting while enqueue

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,19 @@ use std::collections::HashMap;
 
 #[derive(Debug, Copy, Ord, Clone, Eq, PartialEq, PartialOrd)]
 pub struct PQItem {
-    pub value: u32,
-    pub priority: u32,
+    pub value: u8,
+    pub priority: u8,
+}
+
+impl PQItem {
+    pub fn new(value: u8, priority: u8) -> Self {
+        Self { value, priority }
+    }
 }
 
 #[derive(Debug)]
 pub struct PQArray {
     pub p_arr: Vec<PQItem>,
-}
-
-impl PQItem {
-    pub fn new(value: u32, priority: u32) -> Self {
-        Self { value, priority }
-    }
 }
 
 impl PQArray {
@@ -26,7 +26,10 @@ impl PQArray {
     }
 
     pub fn enqueue(&mut self, pq_item: PQItem) {
+        println!("{:?}", &self);
+        &self.p_arr.sort_by(|x, y| x.value.cmp(&y.value));
         self.p_arr.push(pq_item);
+        println!("{:?}", &self);
     }
 
     pub fn peek(&self) -> &PQItem {
@@ -47,6 +50,7 @@ impl PQArray {
 pub fn compress(text_bytes: &Vec<u8>) -> Result<Vec<u8>> {
     //do the compression here
     let mut occur_map: HashMap<u8, u8> = HashMap::new();
+    println!("{:?}", &text_bytes);
 
     for i in text_bytes {
         match occur_map.get(i) {
@@ -57,6 +61,19 @@ pub fn compress(text_bytes: &Vec<u8>) -> Result<Vec<u8>> {
                 occur_map.insert(i.to_owned(), 1);
             }
         }
+    }
+
+    let mut pq_array: PQArray = PQArray::new(PQItem {
+        value: 0,
+        priority: 0,
+    });
+
+    for (k, v) in occur_map {
+        let pq_item: PQItem = PQItem::new(v, k);
+        pq_array.enqueue(pq_item);
+    }
+    for i in pq_array.p_arr {
+        println!("{}:{}", i.value, i.priority);
     }
 
     let compressed_vec: Vec<u8> = Vec::new();
