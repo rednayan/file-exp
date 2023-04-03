ac5186630d1aef9f2c79e8f3bbac03a82307c8d9
nayan <nayansharma1010@gmail.com>
2023-04-03 15:58:22 +0530

    priority queue

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,49 @@
 use anyhow::Result;
 
-pub fn compress(text_bytes: &Vec<u8>) -> Result<Vec<u8>> {
-    //do the compression here
-    let compressed_vec: Vec<u8> = Vec::new();
+#[derive(Debug, Copy, Clone)]
+pub struct PQItem {
+    pub value: u32,
+    pub priority: u32,
+}
 
+#[derive(Debug)]
+pub struct PQArray {
+    pub p_arr: Vec<PQItem>,
+}
+
+impl PQItem {
+    pub fn new(value: u32, priority: u32) -> Self {
+        Self { value, priority }
+    }
+}
+
+impl PQArray {
+    pub fn new(pq_item: PQItem) -> Self {
+        Self {
+            p_arr: vec![pq_item],
+        }
+    }
+
+    pub fn enqueue(&mut self, pq_item: PQItem) {
+        self.p_arr.push(pq_item);
+    }
+
+    // pub fn peek(&self) -> PQItem {
+
+    // }
+}
+
+pub fn create_prio_queue() {
+    let pq_item: PQItem = PQItem::new(2, 3);
+    let pq_item2: PQItem = PQItem::new(4, 5);
+    let mut pq_array: PQArray = PQArray::new(pq_item);
+    PQArray::enqueue(&mut pq_array, pq_item2);
+    println!("{:?}", &pq_array);
+}
+
+pub fn compress(_text_bytes: &Vec<u8>) -> Result<Vec<u8>> {
+    //do the compression here
+    create_prio_queue();
+    let compressed_vec: Vec<u8> = Vec::new();
     Ok(compressed_vec)
 }
